01733fd6a2888fb2cc992d3e68a58814d1142d9c
Devin Cornell <devinj.cornell@gmail.com>
2017-02-17 23:07:00 -0800

    Moved plot functions to be functions in the class.

--- CorrNet.py
+++ CorrNet.py
@@ -36,7 +36,6 @@ class CorrNet(nx.Graph):
             for u in self.node:
                 for v in self.node:
                     if u is not v:
-
                         r, p = stats.spearmanr(df[u], df[v], nan_policy='omit')
                         r, p = float(r), float(p)
                         self.add_edge(u,v, r=r, p=p, ir=1/(r**beta),ar=abs(r))
@@ -56,32 +55,6 @@ class CorrNet(nx.Graph):
         These are graph functions that allow the user to see summary statistics about
         weights that were created.
     '''
-
-    def plotEdgeWeightHist(self, attrName,relBins=10,outlierCutoff=None):
-        edges = np.array(list(nx.get_edge_attributes(self.G,attrName).values()))
-        
-        if outlierCutoff is not None:
-            u = np.median(edges)
-            std = np.sqrt(np.mean(np.absolute(edges-u)))*outlierCutoff
-            edges = edges[np.logical_and(edges > u-std, edges < u+std)]
-
-        #hist,binedges = np.histogram(edges,bins=len(edges)*relBins)
-        nbins = math.floor(relBins*math.log10(np.size(edges)))
-        plt.hist(edges,nbins)
-        plt.show()
-
-    def plotNodeWeightHist(self, attrName,relBins=1.0):
-        edges = nx.get_node_attributes(bn.G,attrName)
-
-        weightList = []
-        for eKey in edges.keys():
-            weightList.append(edges[eKey])
-
-        weightList = np.array(weightList)
-        hist,binedges = np.histogram(weightList,bins=len(edges)*relBins)
-
-        plt.plot(binedges[0:-1],hist)
-        plt.show()
 
     def getRankedNodes(self,attrName,descending=False):
         attr = nx.get_node_attributes(self.G,attrName)
@@ -280,5 +253,29 @@ class CorrNet(nx.Graph):
         return
 
 
+def edge_hist(CN, attr, bins=10, cutoff=None):
+    edges = np.array(list(nx.get_edge_attributes(CN,attr).values()))
+    
+    if cutoff is not None:
+        u = np.median(edges)
+        std = np.sqrt(np.mean(np.absolute(edges-u)))*cutoff
+        edges = edges[np.logical_and(edges > u-std, edges < u+std)]
 
+    #hist,binedges = np.histogram(edges,bins=len(edges)*bins)
+    nbins = math.floor(bins*math.log10(np.size(edges)))
+    plt.hist(edges,nbins)
+    plt.show()
+
+def node_hist(CN, attr, bins=10):
+    edges = nx.get_node_attributes(CN,attr)
+
+    weightList = []
+    for eKey in edges.keys():
+        weightList.append(edges[eKey])
+
+    weightList = np.array(weightList)
+    hist,binedges = np.histogram(weightList,bins=len(edges)*bins)
+
+    plt.plot(binedges[0:-1],hist)
+    plt.show()
 
--- test.py
+++ test.py
@@ -12,12 +12,18 @@ if __name__ == "__main__":
     d['y'] = np.cos(d['t']-0.6)
     d['z'] = np.cos(d['t']-1.3)
     d['w'] = np.cos(d['t']-2.8)
-
+    d['b'] = d['w'] > 0
     df = pd.DataFrame(d)
 
+    # build correlation network
     cn = CorrNet.CorrNet(df)
 
+    # add properties
     ec = nx.betweenness_centrality(cn, weight='ar')
     nx.set_node_attributes(cn,'centrality', ec)
 
+    # show properties
+    CorrNet.edge_hist(cn,'r')
+
+    # save
     nx.write_gexf(cn,'corrnet.gexf')
